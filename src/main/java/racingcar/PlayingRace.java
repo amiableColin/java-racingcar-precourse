50a32905c9f857bd0054aab5341ce7b6caf12fd6
amiableColin <sjlimson@gmail.com>
2024-07-14 21:53:44 +0900

    feat(PlayingRace): Random 모듈을 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange() 메서드로 변경

--- src/main/java/racingcar/PlayingRace.java
+++ src/main/java/racingcar/PlayingRace.java
@@ -1,15 +1,16 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.Random;
+
 public class PlayingRace {
     private final Car[] cars;
     public PlayingRace(Car[] cars) {
         this.cars = cars;
     }
     public void raceStart() {
-        Random r = new Random();
-
         for (Car car : cars) {
-            car.isMove(r.nextInt(10));
+            car.isMove(Randoms.pickNumberInRange(0, 9));
         }
     }
 }
